1a6b03c056c95bc0f4432743b9d857cfc542b996
drparallaxis <44215959+drparallaxis@users.noreply.github.com>
2018-10-17 03:37:54 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+class Graph
+{
+	int id;
+	double x, y, z, length, angle;
+public:
+	Graph()
+	{
+		id = 0;
+		x = y = z = length = angle = 0.0;
+	}
+
+	void convert(){}
+
+	int LengthOfSubgraph(Graph& Sgraph)
+	{
+
+	}
+
+};
+
+int main()
+{
+	
+
+	_getch();
+}
